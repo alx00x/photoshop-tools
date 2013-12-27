775e572cd3fba8921cfccfdbe13030ba64b74416
Axy Mark <axymark@gmail.com>
2013-12-27 22:00:25 +0100

    First commit.

---
+++ groupLayers.jsx
@@ -0,0 +1,143 @@
+﻿#target photoshop
+
+// groupLayers.jsx
+// 
+// Name: groupLayers
+// Version: 0.1
+// 
+// Description:     
+// This script  creates a groop for each of the selected layers.The name
+// of the newly created groups is taken from the layes themselves.
+//  
+
+main();
+
+function main() {
+    if (!documents.length) return;
+    var doc = app.activeDocument;
+    var selLayer = getSelectedLayersIdx();
+    var sLayers = new Array();
+    for (var z in selLayer) {
+        sLayers.push(getLayerID(Number(selLayer[z])));
+    }
+    for (var a in sLayers) {
+        selectLayerById(Number(sLayers[a]));
+        var activeLayer = doc.activeLayer;
+        var newLayerSet = doc.layerSets.add();
+        newLayerSet.move(activeLayer, ElementPlacement.PLACEBEFORE);
+        var newLayerSetIdx = getLayersetIdx();
+        
+        newLayerSet.name = getLayerNameByID(Number(sLayers[a])) + "_comp";
+        
+        selectLayerById(Number(sLayers[a]));
+
+        if (activeLayer instanceof LayerSet) {
+            moveLayerset(activeLayer.name, newLayerSetIdx);
+        } else if (app.activeDocument.activeLayer instanceof ArtLayer ) {
+            activeLayer.move(newLayerSet, ElementPlacement.INSIDE);
+        }
+    }
+};
+
+function selectLayerById(ID, add) {
+    add = (add == undefined) ? add = false : add;
+    var ref = new ActionReference();
+    ref.putIdentifier(charIDToTypeID('Lyr '), ID);
+    var desc = new ActionDescriptor();
+    desc.putReference(charIDToTypeID('null'), ref);
+    if (add) {
+        desc.putEnumerated(stringIDToTypeID('selectionModifier'), stringIDToTypeID('selectionModifierType'), stringIDToTypeID('addToSelection'));
+    }
+    desc.putBoolean(charIDToTypeID('MkVs'), false);
+    executeAction(charIDToTypeID('slct'), desc, DialogModes.NO);
+}
+
+function getLayerNameByID(id) {
+    var ref = new ActionReference();
+    ref.putIdentifier(charIDToTypeID("Lyr "), id);
+    return executeActionGet(ref).getString(charIDToTypeID("Nm  "));
+};
+
+function getLayerIDX(ID) {
+    var ref = new ActionReference();
+    ref.putIdentifier(charIDToTypeID('Lyr '), ID);
+    try {
+        activeDocument.backgroundLayer;
+        return executeActionGet(ref).getInteger(charIDToTypeID("ItmI")) - 1;
+    } catch (e) {
+        return executeActionGet(ref).getInteger(charIDToTypeID("ItmI"));
+    }
+};
+
+function getLayerID(IDX) {
+    var ref = new ActionReference();
+    if (IDX == undefined) {
+        ref.putEnumerated(charIDToTypeID('Lyr '), charIDToTypeID('Ordn'), charIDToTypeID('Trgt'));
+    } else {
+        ref.putIndex(charIDToTypeID('Lyr '), IDX);
+    }
+    var desc = executeActionGet(ref);
+    return desc.getInteger(stringIDToTypeID('layerID'));
+};
+
+function getSelectedLayersIdx() {
+    var selectedLayers = new Array;
+    var ref = new ActionReference();
+    ref.putEnumerated(charIDToTypeID("Dcmn"), charIDToTypeID("Ordn"), charIDToTypeID("Trgt"));
+    var desc = executeActionGet(ref);
+    if (desc.hasKey(stringIDToTypeID('targetLayers'))) {
+        desc = desc.getList(stringIDToTypeID('targetLayers'));
+        var c = desc.count
+        var selectedLayers = new Array();
+        for (var i = 0; i < c; i++) {
+            try {
+                activeDocument.backgroundLayer;
+                selectedLayers.push(desc.getReference(i).getIndex());
+            } catch (e) {
+                selectedLayers.push(desc.getReference(i).getIndex() + 1);
+            }
+        }
+    } else {
+        var ref = new ActionReference();
+        ref.putProperty(charIDToTypeID("Prpr"), charIDToTypeID("ItmI"));
+        ref.putEnumerated(charIDToTypeID("Lyr "), charIDToTypeID("Ordn"), charIDToTypeID("Trgt"));
+        try {
+            activeDocument.backgroundLayer;
+            selectedLayers.push(executeActionGet(ref).getInteger(charIDToTypeID("ItmI")) - 1);
+        } catch (e) {
+            selectedLayers.push(executeActionGet(ref).getInteger(charIDToTypeID("ItmI")));
+        }
+        var vis = app.activeDocument.activeLayer.visible;
+        if (vis == true) app.activeDocument.activeLayer.visible = false;
+        var desc9 = new ActionDescriptor();
+        var list9 = new ActionList();
+        var ref9 = new ActionReference();
+        ref9.putEnumerated(charIDToTypeID('Lyr '), charIDToTypeID('Ordn'), charIDToTypeID('Trgt'));
+        list9.putReference(ref9);
+        desc9.putList(charIDToTypeID('null'), list9);
+        executeAction(charIDToTypeID('Shw '), desc9, DialogModes.NO);
+        if (app.activeDocument.activeLayer.visible == false) selectedLayers.shift();
+        app.activeDocument.activeLayer.visible = vis;
+    }
+    return selectedLayers;
+};
+
+function getLayersetIdx() {
+    var ref = new ActionReference();
+    ref.putEnumerated(charIDToTypeID("Lyr "), charIDToTypeID("Ordn"), charIDToTypeID("Trgt"));
+    var desc = executeActionGet(ref);
+    var idx = desc.getInteger(charIDToTypeID("ItmI"));
+    return idx - 1;
+}
+
+function moveLayerset(groupName, idx) {
+    var desc = new ActionDescriptor();
+    var ref = new ActionReference();
+    ref.putName(charIDToTypeID("Lyr "), groupName);
+    desc.putReference(charIDToTypeID("null"), ref);
+    var ref1 = new ActionReference();
+    ref1.putIndex(charIDToTypeID("Lyr "), idx);
+    desc.putReference(charIDToTypeID("T   "), ref1);
+    desc.putBoolean(charIDToTypeID("Adjs"), false);
+    executeAction(charIDToTypeID("move"), desc, DialogModes.NO);
+};
---
+++ lumaFromLayer.jsx
@@ -0,0 +1,158 @@
+﻿#target photoshop
+
+// lumaFromLayer.jsx
+// 
+// Name: lumaFromLayer
+// Version: 0.1
+// 
+// Description:     
+// This script  creates a luma layer for after effects. It considers both
+// transparancy and masks of currently selected layer.
+//  
+
+main();
+
+function main() {
+    var selectedLayersCount = getSelectedLayersCount();
+    if (selectedLayersCount == 0) {
+        alert("Please select a layer you want to create luma from.");
+    } else if (selectedLayersCount > 1) {
+        alert("Multiple layers are not supported.");
+    } else if (selectedLayersCount == 1) {
+        var selectedLayerName = app.activeDocument.activeLayer.name;
+        var newLayer = app.activeDocument.activeLayer.duplicate();
+        newLayer.name = selectedLayerName + "_luma";
+        selectAbove();
+        addColorOverlay(255,255,255);
+        selectBelow();
+        var bgLayer = makeSolidColorLayer (0,0,0);
+        addToLayerSelection(newLayer);
+        var mergedLayer = activeDocument.activeLayer.merge();
+    }
+}
+
+function getSelectedLayersCount() {
+    var res = new Number();
+    var ref = new ActionReference();
+    ref.putEnumerated(charIDToTypeID("Dcmn"), charIDToTypeID("Ordn"), charIDToTypeID("Trgt"));
+    var desc = executeActionGet(ref);
+    if (desc.hasKey(stringIDToTypeID('targetLayers'))) {
+        desc = desc.getList(stringIDToTypeID('targetLayers'));
+        res = desc.count
+    } else {
+        var vis = app.activeDocument.activeLayer.visible;
+        if (vis == true) app.activeDocument.activeLayer.visible = false;
+        checkVisability();
+        if (app.activeDocument.activeLayer.visible == true) {
+            res = 1;
+        } else {
+            res = 0;
+        }
+        app.activeDocument.activeLayer.visible = vis;
+    }
+    return res;
+}
+
+function checkVisability() {
+    var desc = new ActionDescriptor();
+    var list = new ActionList();
+    var ref = new ActionReference();
+    ref.putEnumerated(charIDToTypeID('Lyr '), charIDToTypeID('Ordn'), charIDToTypeID('Trgt'));
+    list.putReference(ref);
+    desc.putList(charIDToTypeID('null'), list);
+    executeAction(charIDToTypeID('Shw '), desc, DialogModes.NO);
+}
+
+function makeSolidColorLayer (theR, theG, theB) {
+// =======================================================
+var idMk = charIDToTypeID( "Mk  " );
+    var desc3 = new ActionDescriptor();
+    var idnull = charIDToTypeID( "null" );
+        var ref1 = new ActionReference();
+        var idcontentLayer = stringIDToTypeID( "contentLayer" );
+        ref1.putClass( idcontentLayer );
+    desc3.putReference( idnull, ref1 );
+    var idUsng = charIDToTypeID( "Usng" );
+        var desc4 = new ActionDescriptor();
+        var idType = charIDToTypeID( "Type" );
+            var desc5 = new ActionDescriptor();
+            var idClr = charIDToTypeID( "Clr " );
+                var desc6 = new ActionDescriptor();
+                var idRd = charIDToTypeID( "Rd  " );
+                desc6.putDouble( idRd, theR );
+                var idGrn = charIDToTypeID( "Grn " );
+                desc6.putDouble( idGrn, theG );
+                var idBl = charIDToTypeID( "Bl  " );
+                desc6.putDouble( idBl, theB );
+            var idRGBC = charIDToTypeID( "RGBC" );
+            desc5.putObject( idClr, idRGBC, desc6 );
+        var idsolidColorLayer = stringIDToTypeID( "solidColorLayer" );
+        desc4.putObject( idType, idsolidColorLayer, desc5 );
+    var idcontentLayer = stringIDToTypeID( "contentLayer" );
+    desc3.putObject( idUsng, idcontentLayer, desc4 );
+executeAction( idMk, desc3, DialogModes.NO );
+app.activeDocument.activeLayer.name = theR+"R_"+theG+"G_"+theB+"B"
+return app.activeDocument.activeLayer
+};
+
+function addToLayerSelection(layer) {
+    var desc = new ActionDescriptor();
+        var ref = new ActionReference();
+        ref.putName( charIDToTypeID('Lyr '), layer.name );
+    desc.putReference( charIDToTypeID('null'), ref );
+    desc.putEnumerated( stringIDToTypeID('selectionModifier'), stringIDToTypeID('selectionModifierType'), stringIDToTypeID('addToSelection') );
+    desc.putBoolean( charIDToTypeID('MkVs'), false );
+    executeAction( charIDToTypeID('slct'), desc, DialogModes.NO );
+};
+
+function selectAbove() {
+    var idslct = charIDToTypeID("slct");
+    var desc7 = new ActionDescriptor();
+    var idnull = charIDToTypeID("null");
+    var ref5 = new ActionReference();
+    var idLyr = charIDToTypeID("Lyr ");
+    var idOrdn = charIDToTypeID("Ordn");
+    var idFrwr = charIDToTypeID("Frwr");
+    ref5.putEnumerated(idLyr, idOrdn, idFrwr);
+    desc7.putReference(idnull, ref5);
+    var idMkVs = charIDToTypeID("MkVs");
+    desc7.putBoolean(idMkVs, false);
+    executeAction(idslct, desc7, DialogModes.NO);
+}
+
+function selectBelow() {
+    var id3 = charIDToTypeID("slct");
+    var desc2 = new ActionDescriptor();
+    var id4 = charIDToTypeID("null");
+    var ref1 = new ActionReference();
+    var id5 = charIDToTypeID("Lyr ");
+    var id6 = charIDToTypeID("Ordn");
+    var id7 = charIDToTypeID("Bckw");
+    ref1.putEnumerated(id5, id6, id7);
+    desc2.putReference(id4, ref1);
+    var id8 = charIDToTypeID("MkVs");
+    desc2.putBoolean(id8, true);
+    executeAction(id3, desc2, DialogModes.NO);
+}
+
+function addColorOverlay(r,g,b) {
+    var desc = new ActionDescriptor();
+        var ref = new ActionReference();
+        ref.putProperty( charIDToTypeID('Prpr'), charIDToTypeID('Lefx') );
+        ref.putEnumerated( charIDToTypeID('Lyr '), charIDToTypeID('Ordn'), charIDToTypeID('Trgt') );
+    desc.putReference( charIDToTypeID('null'), ref );
+        var effectsDesc = new ActionDescriptor();
+        //effectsDesc.putUnitDouble( charIDToTypeID('Scl '), charIDToTypeID('#Prc'), 333.333333 );
+            var colorfillDesc = new ActionDescriptor();
+            colorfillDesc.putBoolean( charIDToTypeID('enab'), true );
+            colorfillDesc.putEnumerated( charIDToTypeID('Md  '), charIDToTypeID('BlnM'), charIDToTypeID('Nrml') );
+            colorfillDesc.putUnitDouble( charIDToTypeID('Opct'), charIDToTypeID('#Prc'), 100.000000 );
+                var rgbcDesc = new ActionDescriptor();
+                rgbcDesc.putDouble( charIDToTypeID('Rd  '), r );
+                rgbcDesc.putDouble( charIDToTypeID('Grn '), g );
+                rgbcDesc.putDouble( charIDToTypeID('Bl  '), b );
+            colorfillDesc.putObject( charIDToTypeID('Clr '), charIDToTypeID('RGBC'), rgbcDesc );
+        effectsDesc.putObject( charIDToTypeID('SoFi'), charIDToTypeID('SoFi'), colorfillDesc );
+    desc.putObject( charIDToTypeID('T   '), charIDToTypeID('Lefx'), effectsDesc );
+    executeAction( charIDToTypeID('setd'), desc, DialogModes.NO );
+};
